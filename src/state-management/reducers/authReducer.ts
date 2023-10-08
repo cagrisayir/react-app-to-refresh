8b2dc1254ce3418eabbd338f462b717d4ea1221f
Omer Cagri Sayir <omer.sayir@ogr.sakarya.edu.tr>
2023-10-08 16:44:44 +0300

    auth reducer added

---
+++ src/state-management/reducers/authReducer.ts
@@ -0,0 +1,21 @@
+interface LogoutAction {
+	type: 'LOGOUT';
+}
+
+interface LoginAction {
+	type: 'LOGIN';
+	username: string;
+}
+
+type AuthAction = LogoutAction | LoginAction;
+
+const authReducer = (_state: string, action: AuthAction): string => {
+	switch (action.type) {
+		case 'LOGIN':
+			return action.username;
+		case 'LOGOUT':
+			return '';
+	}
+};
+
+export default authReducer;
