87cb6c7674c5a909c9fe1176271176ce5346ffda
Omer Cagri Sayir <omer.sayir@ogr.sakarya.edu.tr>
2023-10-09 12:16:29 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import './App.css';
 import NavBar from './state-management/NavBar';
 import HomePage from './state-management/HomePage';
-import { AuthProvider } from './state-management/auth';
-import { TaskProvider } from './state-management/tasks';
 
 function App() {
 	return (
-		<AuthProvider>
-			<TaskProvider>
-				<NavBar />
-				<HomePage />
-			</TaskProvider>
-		</AuthProvider>
+		<>
+			<NavBar />
+			<HomePage />
+		</>
 	);
 }
 export default App;
